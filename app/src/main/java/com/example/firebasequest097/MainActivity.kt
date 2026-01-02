af6796421345169686d78d36cbbafb4dcc2510e5
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-02 16:55:11 +0700

    Menambahkan izin untuk akses Internet dan status jaringan dan mengubah Scaffold untuk membungkus DataSiswaApp dengan padding di MainActivity.kt

--- app/src/main/java/com/example/firebasequest097/MainActivity.kt
+++ app/src/main/java/com/example/firebasequest097/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firebasequest097.ui.theme.FirebaseQuest097Theme
+import com.example.firebasequest097.view.Navigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirebaseQuest097Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
