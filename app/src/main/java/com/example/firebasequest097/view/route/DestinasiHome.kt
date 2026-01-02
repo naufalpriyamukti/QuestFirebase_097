2cbd462863304ca8563666b47f2cdfe46eb2c0ee
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-02 16:41:25 +0700

    Menambahkan objek DestinasiHome untuk navigasi ke halaman utama

---
+++ app/src/main/java/com/example/firebasequest097/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.firebasequest097.view.route
+
+import com.example.firebasequest097.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
