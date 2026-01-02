5a9ef07fe1361b712856710906a7e68f7083c81d
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-02 16:39:19 +0700

    Menambahkan objek DestinasiEntry untuk navigasi ke entri Siswa

---
+++ app/src/main/java/com/example/firebasequest097/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.firebasequest097.view.route
+
+import com.example.firebasequest097.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
