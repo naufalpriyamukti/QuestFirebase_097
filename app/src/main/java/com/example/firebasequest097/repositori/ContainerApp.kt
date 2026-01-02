943fca9251c04619c39825b385947925d2ad4123
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-02 16:21:03 +0700

    Menambahkan implementasi DefaultContainerApp untuk penyediaan RepositorySiswa

--- app/src/main/java/com/example/firebasequest097/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/firebasequest097/repositori/ContainerApp.kt
@@ -3,3 +3,9 @@ package com.example.firebasequest097.repositori
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
