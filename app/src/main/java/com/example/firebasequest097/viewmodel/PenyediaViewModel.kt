f07cb0ea39250a12584a0b1d9a5b36136a1679d2
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-02 16:32:36 +0700

    Menambahkan ViewModelFactory untuk menginisialisasi HomeViewModel dan EntryViewModel

--- app/src/main/java/com/example/firebasequest097/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebasequest097/viewmodel/PenyediaViewModel.kt
@@ -2,10 +2,16 @@ package com.example.firebasequest097.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.firebasequest097.repositori.AplikasiDataSiswa
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
 
 class PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+    }
 }
