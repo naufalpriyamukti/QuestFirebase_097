d1f893df377bbb1b1d3ed5ce206f4fd01ce6cb99
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-02 16:24:36 +0700

    Menambahkan EntryViewModel dengan dependensi RepositorySiswa dan menambahkan beberapa file viewmodel

---
+++ app/src/main/java/com/example/firebasequest097/viewmodel/EditViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.firebasequest097.viewmodel
+
+class EditViewModel {
+}
---
+++ app/src/main/java/com/example/firebasequest097/viewmodel/EntryViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.firebasequest097.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.firebasequest097.repositori.RepositorySiswa
+
+class EntryViewModel (private val repositorySiswa: RepositorySiswa): ViewModel() {
+}
