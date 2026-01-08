7a26a11049b5562f711646097e7420f8093adca8
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-08 16:02:12 +0700

    implementasi DetailSiswaScreen dan navigasinya

--- app/src/main/java/com/example/firebasequest097/view/Navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/firebasequest097/view/Navigasi/PetaNavigasi.kt
@@ -3,9 +3,11 @@ package com.example.firebasequest097.view.Navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.firebasequest097.view.EntrySiswaScreen
 import com.example.firebasequest097.view.HomeScreen
 import com.example.firebasequest097.view.route.DestinasiDetail
@@ -49,6 +51,12 @@ fun HostNavigasi(
             arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
                 type = NavType.StringType
             })
-        )
+        ){
+            {
+                DetailSiswaScreen(
+                    navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/$it") },
+                    navigateBack = { navController.navigate(DestinasiHome.route) })
+            }
+        }
     }
 }
