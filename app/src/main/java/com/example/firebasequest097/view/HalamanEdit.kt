3277c4ffec3a62d09403c56c3425b29436c74bdc
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-08 16:20:39 +0700

    inisialisasi layar edit siswa menggunakan PenyediaViewModel Factory

---
+++ app/src/main/java/com/example/firebasequest097/view/HalamanEdit.kt
@@ -0,0 +1,19 @@
+package com.example.firebasequest097.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebasequest097.viewmodel.EditViewModel
+import com.example.firebasequest097.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
