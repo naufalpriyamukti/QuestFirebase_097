7de29748434e5233646f074d7eda4d20ed7fa2b5
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-08 16:38:15 +0700

    implementasi sealed interface StatusUIDetail untuk manajemen state detail

--- app/src/main/java/com/example/firebasequest097/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebasequest097/viewmodel/DetailViewModel.kt
@@ -1,4 +1,10 @@
 package com.example.firebasequest097.viewmodel
 
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
 class DetailViewModel {
 }
