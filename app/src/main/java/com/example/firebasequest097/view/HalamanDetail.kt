9bfa858b5080f8cd231590cdfe731f9b79858588
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-01-08 16:10:56 +0700

    Menambahkan fungsi utama DetailSiswaScreen

--- app/src/main/java/com/example/firebasequest097/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebasequest097/view/HalamanDetail.kt
@@ -1,4 +1,21 @@
 package com.example.firebasequest097.view
 
-class HalamanDetail {
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebasequest097.viewmodel.DetailViewModel
+import com.example.firebasequest097.viewmodel.PenyediaViewModel
+
+@OptIn(
+    ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
+{
+
 }
